2e605aa715f32f5c9819cb4439f36cbf5b159b05
patwlan <163450632+patwlan@users.noreply.github.com>
2025-03-13 14:24:23 +0100

    feat: rework agent url. (#49)

--- lib/src/chat/toolbar/tool_bar.dart
+++ lib/src/chat/toolbar/tool_bar.dart
@@ -5,6 +5,8 @@
  */
 
 import 'package:arc_view/src/chat/toolbar/agent_tabs.dart';
+import 'package:arc_view/src/client/notifiers/agent_url_notifier.dart';
+import 'package:arc_view/src/settings/dialogs/edit_agent_url.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smiles/smiles.dart';
@@ -14,9 +16,15 @@ class ToolBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final agentUrl = ref.watch(agentUrlNotifierProvider);
+    final textColor = context.colorScheme.onSurface.withOpacity(0.5);
     return Row(
       children: [
-        const AgentTabs().padByUnits(0, 0, 0, 1).expand(),
+        const AgentTabs().padByUnits(0, 0, 0, 1),
+        'at'.style(color: textColor),
+        agentUrl.url.toString().onPressed(() {
+          showEditAgentUrl(context, ref);
+        }),
       ],
     );
   }
